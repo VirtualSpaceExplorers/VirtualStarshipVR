43fef029fe694e7a159cb209fcec892a049f739a
DeadlyMagikarps <hrumjahn@nevada.unr.edu>
2020-07-24 03:59:38 -0700

    Added Example Class that will be expanded with Interactions namespace. This will of course be changed, but this is to  show layout of scripts. All c# code will live in the Scripts folder. Make subfolders as needed to organize.
    Reduced lighting to dark time. Will need a skybox that is outerspace. Added point light to show darkness.

---
+++ Assets/Scripts/Interactions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Author: DeadlyMagikarps/Kosaki Onodera
+/// Created: 7/24/2020
+/// Description: Namespace to contain all types of interactions. This is a base class.
+/// </summary>
+namespace Interactions {
+    // Variables
+
+    /// <summary>
+    /// Author: DeadlyMagikarps/Kosaki Onodera
+    /// Created: 7/24/2020
+    /// Description: This class has basic information regarding interaction of a door and how it is opened.
+    /// </summary>
+    public class Door : MonoBehaviour
+    {
+        public bool NearDoor()
+        {
+            print("Near Door!");
+            return isNearDoor;
+        }
+
+        public void OpenDoor()
+        {
+            if (isDoor)
+            {
+                // Grab the component, set animation, and "open" the door. Can activate an audio cue or text, or something.
+                isDoorOpened = true;
+            }
+        }
+
+        public void CloseDoor()
+        {
+            // Close the door.
+        }
+
+        protected bool isNearDoor = true;
+        protected bool isDoorOpened;
+        protected bool isDoor = true;
+    }
+}
